6bd127efcbc0cb8840ebd57198a3c1c4cd2bf8a8
Cambri Curtis <cambric4@gmail.com>
2025-11-30 10:46:36 -0700

    added to template

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from '@prisma/config';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // <-- critical
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
+  schema: './prisma/schema.prisma',
+  out: './generated/prisma'
 });
